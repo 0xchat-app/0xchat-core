6939d402b5dcc5a36ccb27c641da95656beef9a9
water783 <wkuangji@outlook.com>
2023-09-13 17:00:23 +0800

    add call kind to gift wrap event

--- lib/src/chat/contacts+calling.dart
+++ lib/src/chat/contacts+calling.dart
@@ -39,11 +39,13 @@ extension Calling on Contacts {
       String toPubkey, SignalingState state, String content) async {
     Completer<OKEvent> completer = Completer<OKEvent>();
     Event? event;
+    int kind = 25051;
     switch (state) {
       case SignalingState.disconnect:
         event = Nip100.close(toPubkey, content, privkey);
         break;
       case SignalingState.offer:
+        kind = 25050;
         event = Nip100.offer(toPubkey, content, privkey);
         break;
       case SignalingState.answer:
@@ -58,7 +60,7 @@ extension Calling on Contacts {
 
     /// 60s timeout for calling event
     Event encodeEvent = await Nip24.encode(event, toPubkey, privkey,
-        expiration: currentUnixTimestampSeconds() + 60);
+        expiration: currentUnixTimestampSeconds() + 60, kind: kind);
     Connect.sharedInstance.sendEvent(encodeEvent,
         sendCallBack: (ok, relay) async {
       if (!completer.isCompleted) completer.complete(ok);
