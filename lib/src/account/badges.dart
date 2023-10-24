60cc1cf9398f0f13068ade84b01c928a926763dd
water783 <wkuangji@outlook.com>
2023-10-24 13:08:45 +0800

    optimize badge view

--- lib/src/account/badges.dart
+++ lib/src/account/badges.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:nostr_core_dart/nostr.dart';
 import 'package:chatcore/chat-core.dart';
+import 'package:sqflite_sqlcipher/sqflite.dart';
 
 typedef BadgeAwardsCallBack = void Function(List<BadgeAward?>?);
 
@@ -11,7 +12,90 @@ class BadgesHelper {
   factory BadgesHelper() => sharedInstance;
   static final BadgesHelper sharedInstance = BadgesHelper._internal();
 
+  String badgeSubscription = '';
+  final String serverPubkey =
+      '093dff31a87bbf838c54fd39ff755e72b38bd6b7975c670c0f2633fa7c54ddd0';
+
   Map<String, BadgeDB> badgeInfos = {};
+  Map<String, BadgeDB> chatBadges = {};
+
+  Future<void> init() async {
+    await _loadChatBadges();
+    _updateSubscriptions();
+    // subscript friend requests
+    Connect.sharedInstance.addConnectStatusListener((relay, status) async {
+      if (status == 1 && Account.sharedInstance.me != null) {
+        _updateSubscriptions(relay: relay);
+      }
+    });
+  }
+
+  void _updateSubscriptions({String? relay}) {
+    if (badgeSubscription.isNotEmpty) {
+      Connect.sharedInstance.closeRequests(badgeSubscription, relay: relay);
+    }
+
+    Map<String, List<Filter>> subscriptions = {};
+
+    if (relay == null) {
+      for (String relayURL in Connect.sharedInstance.relays()) {
+        int commonMessageUntil =
+            Relays.sharedInstance.getCommonMessageUntil(relayURL);
+        Filter f = Filter(
+            kinds: [8], authors: [serverPubkey], since: commonMessageUntil);
+
+        subscriptions[relayURL] = [f];
+      }
+    } else {
+      int commonMessageUntil =
+          Relays.sharedInstance.getCommonMessageUntil(relay);
+      Filter f = Filter(
+          kinds: [8], authors: [serverPubkey], since: commonMessageUntil);
+      subscriptions[relay] = [f];
+    }
+
+    badgeSubscription = Connect.sharedInstance.addSubscriptions(subscriptions,
+        eventCallBack: (event, relay) async {
+      BadgeAward? badgeAward = Nip58.getBadgeAward(event);
+      if (badgeAward != null) {
+        BadgeDB? badgeDB = _get0xchatBadgeInfo(badgeAward.identifies);
+        print('_updateSubscriptions ${badgeDB?.name}');
+        if (badgeDB != null) {
+          // save to DB
+          BadgeAwardDB badgeAwardDB = badgeAwardToBadgeAwardDB(badgeAward);
+          badgeAwardDB.badgeId = badgeDB.id;
+          await DB.sharedInstance.insert<BadgeAwardDB>(badgeAwardDB,
+              conflictAlgorithm: ConflictAlgorithm.ignore);
+          // cache to user DB
+          await _addUserBadgeToDB(badgeAwardDB.badgeOwner, badgeDB.id);
+        }
+      }
+    });
+  }
+
+  BadgeDB? _get0xchatBadgeInfo(String? d) {
+    return chatBadges[d];
+  }
+
+  Future<void> _loadChatBadges() async {
+    String jsonString =
+        '{\"code\":\"000000\",\"message\":\"Success\",\"data\":[{\"identifies\":\"aa330e14a58c5ada40ee0cea7f4c3121\",\"badgeId\":\"00003f2b93a5a3888fff0d251d270ca82f757a0d5efd556070a036ca8be8e820\",\"badgeName\":\"Egg\",\"badgeImageUrl\":\"https://www.0xchat.com/ipfs/QmWvaqDS5m35577sXZDae7m6fEU6SidznSwdbWndAEmUPe\",\"thumbUrl\":\"https://www.0xchat.com/ipfs/QmXgDVHfUcK5dXeQDKyX7R4DggTTkL9snEc61SXMqSNy4e\",\"creator\":\"0xchat\",\"description\":\"Earn the 0xchat Egg badge with cumulative donations of 2.1k sats. Your humble beginning sets the stage for a journey of a thousand miles.\",\"benefits\":[\"4-digit 0xchat DNS\"],\"benefitsIcon\":[\"https://www.0xchat.com/ipfs/QmU1CAcYjoVJq1DeeN66Yn4T2WSH8n1e1piHJ9BbQ1giyK\"],\"creatorAbout\":\"0xchat is a decentralized chatting app built on Nostr protocol. It prioritizes privacy with features like private key login, encrypted private chats and contacts, coupled with an open communication platform through public channels.\",\"howToGet\":\"Cumulative donations of 2.1k sats\"},{\"identifies\":\"fafec3cd6d8c04ad424ca82ed54e9afb\",\"badgeId\":\"00000f0951248ace7d69be0ed136a069f1d1021443b03e81ffbabda4e53bda9e\",\"badgeName\":\"Hatchling\",\"badgeImageUrl\":\"https://www.0xchat.com/ipfs/QmUTQT3djB92u4UEBtcjup7dhRhPAsGc3uLeEhRcforuDH\",\"thumbUrl\":\"https://www.0xchat.com/ipfs/QmTchyNpgxC4gFZ2P3UEHDKBwMrta9WV8Xp9uy4rwdyWNC\",\"creator\":\"0xchat\",\"description\":\"Garner the 0xchat Hatchling badge with cumulative donations of 4.2k sats. Your continued support hatches new opportunities and fosters breakthroughs.\",\"benefits\":[\"4-digit 0xchat DNS\"],\"benefitsIcon\":[\"https://www.0xchat.com/ipfs/QmU1CAcYjoVJq1DeeN66Yn4T2WSH8n1e1piHJ9BbQ1giyK\"],\"creatorAbout\":\"0xchat is a decentralized chatting app built on Nostr protocol. It prioritizes privacy with features like private key login, encrypted private chats and contacts, coupled with an open communication platform through public channels.\",\"howToGet\":\"Cumulative donations of 4.2k sats\"},{\"identifies\":\"115e7ac9cb28ccc172b9fde8b5ef4a50\",\"badgeId\":\"00002295d1ef76eee9a8d8d04cb2009483acedb452814e040a949555210f928e\",\"badgeName\":\"Chick\",\"badgeImageUrl\":\"https://www.0xchat.com/ipfs/QmaosfucQmDWt7hHnb1qgcgN8xREzwnHzYu9ysVeBweE4p\",\"thumbUrl\":\"https://www.0xchat.com/ipfs/QmWoyYV1DxK1A89M89YMSNSVgj2MgxipTL1ZASLyMJ7a8w\",\"creator\":\"0xchat\",\"description\":\"Attain the 0xchat Chick badge with cumulative donations of 21k sats. Like a chick learning to walk, your contributions enable steady progress.\",\"benefits\":[\"3-digit 0xchat DNS\"],\"benefitsIcon\":[\"https://www.0xchat.com/ipfs/QmU1CAcYjoVJq1DeeN66Yn4T2WSH8n1e1piHJ9BbQ1giyK\"],\"creatorAbout\":\"0xchat is a decentralized chatting app built on Nostr protocol. It prioritizes privacy with features like private key login, encrypted private chats and contacts, coupled with an open communication platform through public channels.\",\"howToGet\":\"Cumulative donations of 21k sats\"},{\"identifies\":\"8aeb9c5cd440dadbdeb6daa4fd97b763\",\"badgeId\":\"00001e93260c429660e39abd7cec733f9f3d9278bb7b2bbd8a3fff98d94a02bb\",\"badgeName\":\"Adolescent\",\"badgeImageUrl\":\"https://www.0xchat.com/ipfs/QmU5SUQu3dGrPE4jibkBYuXsE1YLjRJuFUPv894dkqDaVr\",\"thumbUrl\":\"https://www.0xchat.com/ipfs/QmXaP3fPLLtXS5a9iZrXgAvLmpjtY7FjdLj4VXRhKTgVCE\",\"creator\":\"0xchat\",\"description\":\"Acquire the 0xchat Adolescent badge with cumulative donations of 42k sats. Your speedy commitment propels us towards our mission.\",\"benefits\":[\"3-digit 0xchat DNS\"],\"benefitsIcon\":[\"https://www.0xchat.com/ipfs/QmU1CAcYjoVJq1DeeN66Yn4T2WSH8n1e1piHJ9BbQ1giyK\"],\"creatorAbout\":\"0xchat is a decentralized chatting app built on Nostr protocol. It prioritizes privacy with features like private key login, encrypted private chats and contacts, coupled with an open communication platform through public channels.\",\"howToGet\":\"Cumulative donations of 42k sats\"},{\"identifies\":\"ceba4defe16e22c5ca2d9bedcaec430e\",\"badgeId\":\"00000f7a835198691c5cd6f024fdfcf2ab82ef147160bf966ba5706271baa003\",\"badgeName\":\"Mature\",\"badgeImageUrl\":\"https://www.0xchat.com/ipfs/QmNuPsPojLxiBbPVEwYGCi8BqYQEkTemD9QLT4wWaBUP2u\",\"thumbUrl\":\"https://www.0xchat.com/ipfs/QmcKPMr6JX1ewUJzX6vPWPsuE9zDPg1ZiXhecryi57Tcgt\",\"creator\":\"0xchat\",\"description\":\"Receive the 0xchat Mature badge with cumulative donations of 210k sats. Your significant support, like a full-grown ostrich, matures into incredible growth.\",\"benefits\":[\"2-digit 0xchat DNS\"],\"benefitsIcon\":[\"https://www.0xchat.com/ipfs/QmU1CAcYjoVJq1DeeN66Yn4T2WSH8n1e1piHJ9BbQ1giyK\"],\"creatorAbout\":\"0xchat is a decentralized chatting app built on Nostr protocol. It prioritizes privacy with features like private key login, encrypted private chats and contacts, coupled with an open communication platform through public channels.\",\"howToGet\":\"Cumulative donations of 210k sats\"},{\"identifies\":\"1882297acefba82b14317c7c8e8fe28d\",\"badgeId\":\"000001a0ec0e7908caed173a82f68e93f8d791f7a045da0bf622c7bee0b72dee\",\"badgeName\":\"Geeky\",\"badgeImageUrl\":\"https://www.0xchat.com/ipfs/QmVKujbWMQCN6LfW2MvMo6B6XPrPAoA1nuwPf5YiCJeiuB\",\"thumbUrl\":\"https://www.0xchat.com/ipfs/Qme4TT55HCUbrx6SAZd9uwt5uotzekxFfmYJBNVhvFGEgN\",\"creator\":\"0xchat\",\"description\":\"Obtain the 0xchat Geeky badge with cumulative donations of 420k sats. You are not just a superhero, but a geeky one, powering innovation across our work.\",\"benefits\":[\"2-digit 0xchat DNS\"],\"benefitsIcon\":[\"https://www.0xchat.com/ipfs/QmU1CAcYjoVJq1DeeN66Yn4T2WSH8n1e1piHJ9BbQ1giyK\"],\"creatorAbout\":\"0xchat is a decentralized chatting app built on Nostr protocol. It prioritizes privacy with features like private key login, encrypted private chats and contacts, coupled with an open communication platform through public channels.\",\"howToGet\":\"Cumulative donations of 420k sats\"}]}';
+    final List datas = json.decode(jsonString)['data'];
+    for (var data in datas) {
+      BadgeDB badgeDB = BadgeDB(
+          id: data['badgeId'],
+          name: data['badgeName'],
+          description: data['description'],
+          image: data['badgeImageUrl'],
+          thumb: data['thumbUrl'],
+          creator: serverPubkey,
+          d: data['identifies']);
+      chatBadges[badgeDB.d] = badgeDB;
+      badgeInfos[badgeDB.id] = badgeDB;
+      DB.sharedInstance.insert<BadgeDB>(badgeDB,
+          conflictAlgorithm: ConflictAlgorithm.ignore);
+    }
+  }
 
   static BadgeDB _badgeToBadgeDB(Badge badge) {
     return BadgeDB(
@@ -205,20 +289,14 @@ class BadgesHelper {
 
   static Future<List<BadgeAwardDB?>?> getUserBadgesFromDB(
       String userPubkey) async {
-    Completer<List<BadgeAwardDB?>?> completer =
-        Completer<List<BadgeAwardDB?>?>();
-
     UserDB? userDB = await Account.sharedInstance.getUserInfo(userPubkey);
     if (userDB != null &&
         userDB.badgesList != null &&
         userDB.badgesList!.isNotEmpty) {
-      List<BadgeAwardDB?> badges =
-          await getBadgeAwardInfosFromDB(userDB.badgesList!, userPubkey);
-      if (!completer.isCompleted) completer.complete(badges);
+      return await getBadgeAwardInfosFromDB(userDB.badgesList!, userPubkey);
     } else {
-      if (!completer.isCompleted) completer.complete(null);
+      return null;
     }
-    return completer.future;
   }
 
   static BadgeAwardDB badgeAwardToBadgeAwardDB(BadgeAward award) {
@@ -240,16 +318,29 @@ class BadgesHelper {
     }
   }
 
-  static Future<OKEvent> setProfileBadges(
-      List<String> badgeIds) async {
+  static Future<void> _addUserBadgeToDB(
+      String userPubkey, String addBadge) async {
+    UserDB? userDB = await Account.sharedInstance.getUserInfo(userPubkey);
+    if (userDB != null && addBadge.isNotEmpty) {
+      if (userDB.badgesList != null && !userDB.badgesList!.contains(addBadge)) {
+        userDB.badgesList!.add(addBadge);
+        await DB.sharedInstance.insert<UserDB>(userDB);
+      } else if (userDB.badgesList == null) {
+        userDB.badgesList = [addBadge];
+        await DB.sharedInstance.insert<UserDB>(userDB);
+      }
+    }
+  }
+
+  static Future<OKEvent> setProfileBadges(List<String> badgeIds) async {
     Completer<OKEvent> completer = Completer<OKEvent>();
 
     List<BadgeDB?> badges = await getBadgeInfosFromDB(badgeIds);
     List<BadgeAward> badgeAwards = [];
     for (BadgeDB? badgeDB in badges) {
       if (badgeDB != null) {
-        List<BadgeAwardDB?> map =
-            await getBadgeAwardFromDB(badgeDB.d, badgeDB.creator, Account.sharedInstance.currentPubkey);
+        List<BadgeAwardDB?> map = await getBadgeAwardFromDB(
+            badgeDB.d, badgeDB.creator, Account.sharedInstance.currentPubkey);
         if (map.isNotEmpty && map[0] != null) {
           BadgeAwardDB? db = map[0];
           BadgeAward badgeAward = BadgeAward(
@@ -263,7 +354,8 @@ class BadgesHelper {
       }
     }
     if (badgeAwards.isNotEmpty) {
-      Event event = Nip58.setProfileBadges(badgeAwards, Account.sharedInstance.currentPrivkey);
+      Event event = Nip58.setProfileBadges(
+          badgeAwards, Account.sharedInstance.currentPrivkey);
       Connect.sharedInstance.sendEvent(event, sendCallBack: (ok, relay) async {
         if (ok.status) {
           UserDB? userDB = Account.sharedInstance.me;
