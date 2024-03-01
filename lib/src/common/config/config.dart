197abc723740e555d3083b43f5891106a73b0681
water783 <wkuangji@outlook.com>
2024-03-01 14:32:09 +0800

    add httpsdns

--- lib/src/common/config/config.dart
+++ lib/src/common/config/config.dart
@@ -25,12 +25,14 @@ class Config {
   ];
   // host config
   Map<String, String> hostConfig = {
-    "wss://relay.0xchat.com": "ws://54.179.158.105:6969"
+    "wss://relay.0xchat.com": "ws://54.179.158.105:6969",
+    "https://www.0xchat.com": "54.179.158.105:9601",
   };
   final String _serverPubkey =
       '093dff31a87bbf838c54fd39ff755e72b38bd6b7975c670c0f2633fa7c54ddd0';
   final String configD = '0xchat-config';
-  final String chatHost = 'wss://relay.0xchat.com';
+  final String wssHost = 'wss://relay.0xchat.com';
+  final String httpHost = 'https://www.0xchat.com';
 
   Future<void> initConfig() async {
     await _loadConfig();
@@ -108,8 +110,10 @@ class Config {
     String? json = configs[configD]?.configJson;
     if (json != null) {
       Map map = jsonDecode(json);
-      String? dns = map['dns'];
-      hostConfig[chatHost] = 'ws://${dns ?? '54.179.158.105:6969'}';
+      String? wssdns = map['wssdns'];
+      hostConfig[wssHost] = 'ws://${wssdns ?? '54.179.158.105:6969'}';
+      String? httpsdns = map['httpsdns'];
+      hostConfig[httpHost] = httpsdns ?? '54.179.158.105:9601';
       List<String>? channels = (map['recommendChannels'] as List<dynamic>?)
           ?.map((item) => item.toString())
           .toList();
