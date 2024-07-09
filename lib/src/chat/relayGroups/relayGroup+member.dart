ee6df24ce658114711a73a39a29ef1ada5dafc84
water783 <wkuangji@outlook.com>
2024-07-09 11:35:38 +0800

    callback after join/remove group

--- lib/src/chat/relayGroups/relayGroup+member.dart
+++ lib/src/chat/relayGroups/relayGroup+member.dart
@@ -58,6 +58,7 @@ extension EMember on RelayGroup {
     RelayGroupDB? groupDB = myGroups[groupId];
     if (groupDB == null) return OKEvent(groupId, false, 'group not exit');
     myGroups.remove(groupId);
+    myGroupsUpdatedCallBack?.call();
     return await syncMyGroupListToRelay();
   }
 
@@ -66,6 +67,7 @@ extension EMember on RelayGroup {
     String groupId = simpleGroups.groupId;
     if (groups.containsKey(groupId) && !myGroups.containsKey(groupId)) {
       myGroups[groupId] = groups[groupId]!;
+      myGroupsUpdatedCallBack?.call();
       return await syncMyGroupListToRelay();
     }
     return OKEvent(groupId, false, 'group not found');
